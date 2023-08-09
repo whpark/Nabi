4341f4f162c9f7e37777d57164f90657868fb6d4
whpark <45619217+whpark@users.noreply.github.com>
2023-08-09 16:42:45 +0900

    .

--- src/Nabi/main.cpp
+++ src/Nabi/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include "pch.h"
 #include "scn/scn.h"
 #include "MainWnd.h"
-//#include "gtl/win_util/EnvironmentVairable.h"
+#include "gtl/win/EnvironmentVariable.h"
 
 //OPENCV_IO_MAX_IMAGE_PIXELS=1099511627776
 int main(int argc, char* argv[]) {
@@ -30,31 +30,31 @@ int main(int argc, char* argv[]) {
 			"openCV - Large Image",
 			"openCV - Large Image not supported.",
 			QMessageBox::Close);
-		//if (r == QMessageBox::Yes) {
-		//	// export environmet variable
+		if (r == QMessageBox::Yes) {
+			// export environmet variable
 
-		//	gtl::qt::xWaitCursor wc;
+			gtl::qt::xWaitCursor wc;
 
-		//	//using namespace gtl::win_util;
-		//	//xEnvironmentVariable var(xEnvironmentVariable::eSCOPE::LOCAL_MACHINE);
-		//	//if (!var)
-		//	//	var.Open(xEnvironmentVariable::eSCOPE::CURRENT_USER);
-		//	//if (var) {
-		//	//	var.Set(L"OPENCV_IO_MAX_IMAGE_PIXELS", std::format(L"{}", 0x01ull << 40));
-		//	//	var.Set(L"OPENCV_IO_MAX_IMAGE_WIDTH", std::format(L"{}", 0x01ull << 24));
-		//	//	var.Set(L"OPENCV_IO_MAX_IMAGE_HEIGHT", std::format(L"{}", 0x01ull << 24));
-		//	//	var.Broadcast();
-		//	//}
+			using namespace gtl::win;
+			xEnvironmentVariable var(xEnvironmentVariable::eSCOPE::LOCAL_MACHINE);
+			if (!var)
+				var.Open(xEnvironmentVariable::eSCOPE::CURRENT_USER);
+			if (var) {
+				var.Set(L"OPENCV_IO_MAX_IMAGE_PIXELS", std::format(L"{}", 0x01ull << 40));
+				var.Set(L"OPENCV_IO_MAX_IMAGE_WIDTH", std::format(L"{}", 0x01ull << 24));
+				var.Set(L"OPENCV_IO_MAX_IMAGE_HEIGHT", std::format(L"{}", 0x01ull << 24));
+				var.Broadcast();
+			}
 
-		//	QMessageBox::warning(nullptr,
-		//		"openCV - Large Image",
-		//		"Please, Restart Program",
-		//		QMessageBox::Close);
+			QMessageBox::warning(nullptr,
+				"openCV - Large Image",
+				"Please, Restart Program",
+				QMessageBox::Close);
 
-		//	//auto h = RegisterApplicationRestart(L"", 0);
+			//auto h = RegisterApplicationRestart(L"", 0);
 
-		//	return 0;
-		//}
+			return 0;
+		}
 	}
 
 	return a.exec();
